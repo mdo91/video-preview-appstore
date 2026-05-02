08f2efdb33ab163a1f82c29bc0d183195b37c380
Mahmut Aoata <hesoka_mxx@yahoo.com>
2026-05-02 08:00:40 +0300

    Initial Commit

---
+++ AppStorePreviewConverter/AppStorePreviewConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorePreviewConverterApp.swift
+//  AppStorePreviewConverter
+//
+//  Created by Mahmoud Aoata on 02/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct AppStorePreviewConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
